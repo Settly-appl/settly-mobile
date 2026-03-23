d1ff474befb19cdde188bb25d28c187dc3b23b12
michal <michu.romanczuk@gmail.com>
2026-03-23 12:32:12 +0100

    Dark mode implementation

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,52 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:settly_mobile/pages/home_page.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:settly_mobile/projectColors/app_colors.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
-    statusBarIconBrightness: Brightness.dark,
-  ));
   await initializeDateFormatting('pl', null);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // Ta metoda pozwoli nam znaleźć stan MyApp w dowolnym miejscu aplikacji
+  static _MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>()!;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Domyślnie ustawiamy na systemowy
+  ThemeMode _themeMode = ThemeMode.system;
+
+  // Funkcja do zmiany motywu
+  void changeTheme(ThemeMode themeMode) {
+    setState(() {
+      _themeMode = themeMode;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: AppColors.scaffold(false),
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: AppColors.scaffold(true),
+      ),
+      themeMode: _themeMode,
+      home: const HomePage(),
+    );
   }
 }
